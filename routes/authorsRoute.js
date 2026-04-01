c5ae4930a8ee9d28e4a73696ff7c3ae7ab4a2335
Donald2023-source <donalddyusuf@gmail.com>
2026-04-01 23:17:03 +0100

    feat: delete author

--- routes/authorsRoute.js
+++ routes/authorsRoute.js
@@ -4,6 +4,7 @@ const {
   fetchAllAuthors,
   fetchAuthorById,
   updateAuthor,
+  deleteAuthor,
 } = require("../controllers/authorController");
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.post("/create", createAuthor);
 router.get("/fetch-all", fetchAllAuthors);
 router.get("/getAuthor/:id", fetchAuthorById);
 router.put("/update/:id", updateAuthor);
+router.delete("/delete/:id", deleteAuthor);
 
 module.exports = router;
