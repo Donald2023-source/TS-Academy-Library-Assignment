d88d13589afff69582f5672e2fa3b47b884d4aca
Donald2023-source <donalddyusuf@gmail.com>
2026-04-01 23:15:03 +0100

    feat: update author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -43,3 +43,84 @@ exports.fetchAllAuthors = async (req, res) => {
     });
   }
 };
+
+exports.fetchAuthorById = async (req, res) => {
+  try {
+    const authorId = req.params.id;
+
+    if (!authorId) {
+      return res.status(400).json({
+        message: "Author ID is required",
+      });
+    }
+
+    const author = await Author.findById(authorId);
+
+    if (!author) {
+      return res.status(404).json({
+        message: "Author not found",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Author fetched successfully",
+      data: author,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+exports.updateAuthor = async (req, res) => {
+  try {
+    const { name, bio } = req.body;
+    const authorId = req.params.id;
+
+    if (!authorId || !name || !bio) {
+      return res.status(400).json({
+        message: "Something is missing",
+      });
+    }
+
+    const author = await Author.findById(authorId);
+
+    if (!author) {
+      return res.status(404).json({
+        message: "Author not found",
+      });
+    }
+
+    author.name = name;
+    author.bio = bio;
+
+    await author.save();
+
+    return res.status(200).json({
+      message: "Author fetched successfully",
+      data: author,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+exports.deleteAuthor = async (req, res) => {
+  try {
+    const authorId = req.params.id;
+
+    if (!authorId) {
+      return res.status(404).json({ message: "ID not found!", status: 404 });
+    }
+
+    await Author.findByIdAndDelete(authorId);
+    res.status(200).json({ message: "Author Deleted Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server error", status: 500 });
+  }
+};
--- routes/authorsRoute.js
+++ routes/authorsRoute.js
@@ -2,11 +2,15 @@ const express = require("express");
 const {
   createAuthor,
   fetchAllAuthors,
+  fetchAuthorById,
+  updateAuthor,
 } = require("../controllers/authorController");
 
 const router = express.Router();
 
 router.post("/create", createAuthor);
 router.get("/fetch-all", fetchAllAuthors);
+router.get("/getAuthor/:id", fetchAuthorById);
+router.put("/update/:id", updateAuthor);
 
 module.exports = router;
